d5cd9c0bdba9cdd5ba65ad832157fd7f7fd22665
Sui Caedere <szhigau@yandex.ru>
2021-08-15 13:01:19 +0300

    add mutation-observer

--- script.user.js
+++ script.user.js
@@ -17,17 +17,24 @@ const INDEX_ORE = 4;
 const INDEX_URANIUM = 11;
 const INDEX_DIAMONDS = 15;
 
-(function() {
+(function () {
     'use strict';
 
-    window.addEventListener('hashchange', e => {
-        checkForUrl(e.newURL);
+    const target = document.getElementById('header_slide_inner');
+    const observer = new MutationObserver(mutations => {
+        for (let mutation of mutations) {
+            if (mutation.addedNodes.length > 0) {
+                checkForUrl(window.location.href);
+            }
+        }
     });
+
+    observer.observe(target, { childList: true });
 })();
 
 function checkForUrl(url) {
     const URL = url.match(/map\/details\/\d+/);
-    if(URL) {
+    if (URL) {
         addButton();
     }
 }
@@ -61,7 +68,7 @@ async function getMarketPrice(index) {
     const arr = result.slice(result.indexOf('[[') + 1, -3).replace(/\[\d+\, /g, '').split('], ');
     const average = (arr.reduce((a, b) => parseFloat(a) + parseFloat(b), 0) / arr.length).toFixed(2);
     let name;
-    switch(index) {
+    switch (index) {
         case INDEX_OIL:
             name = 'нефти';
             break;
@@ -95,86 +102,87 @@ function calcResources() {
     const hospital = wrapper.match(/Госпиталь\: (?<hospital>\d+)/).groups.hospital;
     const mbase = wrapper.match(/Военная база\: (?<mbase>\d+)/).groups.mbase;
     const school = wrapper.match(/Школа\: (?<school>\d+)/).groups.school;
-    const missile = wrapper.match(/ПВО\: (?<missile>\d+)/).groups.missile;
+    const missile = wrapper.match(/ПВО\: (?<missile>\d+)/) ? wrapper.match(/ПВО\: (?<missile>\d+)/).groups.missile : 0;
     const port = wrapper.match(/Порт\: (?<port>\d+)/) ? wrapper.match(/Порт\: (?<port>\d+)/).groups.port : 0;
     const electro = wrapper.match(/Электростанция\: (?<electro>\d+)/).groups.electro;
     const cosmo = wrapper.match(/Космодром\: (?<cosmo>\d+)/).groups.cosmo;
-    const aero = wrapper.match(/Аэропорт\: (?<aero>\d+)/).groups.aero;
+    const aero = wrapper.match(/Аэропорт\: (?<aero>\d+)/) ? wrapper.match(/Аэропорт\: (?<aero>\d+)/).groups.aero : 0;
     const hfound = wrapper.match(/Жилой фонд\: (?<hfound>\d+)/).groups.hfound;
+    const gas = wrapper.match(/Заправочная станция\: (?<gas>\d+)/) ? wrapper.match(/Заправочная станция\: (?<aero>\d+)/).groups.gas : 0;
 
     console.log(`Постройки региона: Госпиталь ${hospital}, Военная база ${mbase}, Школа ${school}, ПВО ${missile}, Порт ${port}, Электростанция ${electro}, Космодром ${cosmo}, Аэропорт ${aero}, Жилой фонд ${hfound}`);
 
     for (let i = 1; i <= hospital; i++) {
-        money += Math.round(Math.pow(i*300, 1.5));
-        gold += Math.round(Math.pow(i*2160, 1.5));
-        oil += Math.round(Math.pow(i*160, 1.5));
-        ore += Math.round(Math.pow(i*90, 1.5));
+        money += Math.round(Math.pow(i * 300, 1.5));
+        gold += Math.round(Math.pow(i * 2160, 1.5));
+        oil += Math.round(Math.pow(i * 160, 1.5));
+        ore += Math.round(Math.pow(i * 90, 1.5));
     }
 
     for (let i = 1; i <= mbase; i++) {
-        money += Math.round(Math.pow(i*300, 1.5));
-        gold += Math.round(Math.pow(i*2160, 1.5));
-        oil += Math.round(Math.pow(i*160, 1.5));
-        ore += Math.round(Math.pow(i*90, 1.5));
+        money += Math.round(Math.pow(i * 300, 1.5));
+        gold += Math.round(Math.pow(i * 2160, 1.5));
+        oil += Math.round(Math.pow(i * 160, 1.5));
+        ore += Math.round(Math.pow(i * 90, 1.5));
     }
 
     for (let i = 1; i <= school; i++) {
-        money += Math.round(Math.pow(i*300, 1.5));
-        gold += Math.round(Math.pow(i*2160, 1.5));
-        oil += Math.round(Math.pow(i*160, 1.5));
-        ore += Math.round(Math.pow(i*90, 1.5));
+        money += Math.round(Math.pow(i * 300, 1.5));
+        gold += Math.round(Math.pow(i * 2160, 1.5));
+        oil += Math.round(Math.pow(i * 160, 1.5));
+        ore += Math.round(Math.pow(i * 90, 1.5));
     }
 
     for (let i = 1; i <= missile; i++) {
-        money += Math.round(Math.pow(i*1000, 1.5));
-        gold += Math.round(Math.pow(i*180, 1.5));
-        oil += Math.round(Math.pow(i*10, 1.5));
-        ore += Math.round(Math.pow(i*10, 1.5));
-        diamonds += Math.round(Math.pow(i*10, 0.7));
+        money += Math.round(Math.pow(i * 1000, 1.5));
+        gold += Math.round(Math.pow(i * 180, 1.5));
+        oil += Math.round(Math.pow(i * 10, 1.5));
+        ore += Math.round(Math.pow(i * 10, 1.5));
+        diamonds += Math.round(Math.pow(i * 10, 0.7));
     }
 
     for (let i = 1; i <= port; i++) {
-        money += Math.round(Math.pow(i*1000, 1.5));
-        gold += Math.round(Math.pow(i*180, 1.5));
-        oil += Math.round(Math.pow(i*10, 1.5));
-        ore += Math.round(Math.pow(i*10, 1.5));
-        diamonds += Math.round(Math.pow(i*10, 0.7));
+        money += Math.round(Math.pow(i * 1000, 1.5));
+        gold += Math.round(Math.pow(i * 180, 1.5));
+        oil += Math.round(Math.pow(i * 10, 1.5));
+        ore += Math.round(Math.pow(i * 10, 1.5));
+        diamonds += Math.round(Math.pow(i * 10, 0.7));
     }
 
     for (let i = 1; i <= electro; i++) {
-        money += Math.round(Math.pow(i*2000, 1.5));
-        gold += Math.round(Math.pow(i*90, 1.5));
-        oil += Math.round(Math.pow(i*25, 1.5));
-        ore += Math.round(Math.pow(i*25, 1.5));
-        diamonds += Math.round(Math.pow(i*5, 0.7));
-        uranium += Math.round(Math.pow(i*20, 1.5));
+        money += Math.round(Math.pow(i * 2000, 1.5));
+        gold += Math.round(Math.pow(i * 90, 1.5));
+        oil += Math.round(Math.pow(i * 25, 1.5));
+        ore += Math.round(Math.pow(i * 25, 1.5));
+        diamonds += Math.round(Math.pow(i * 5, 0.7));
+        uranium += Math.round(Math.pow(i * 20, 1.5));
     }
 
     for (let i = 1; i <= cosmo; i++) {
-        money += Math.round(Math.pow(i*6000, 1.5));
-        gold += Math.round(Math.pow(i*180, 1.5));
-        oil += Math.round(Math.pow(i*30, 1.5));
-        ore += Math.round(Math.pow(i*25, 1.5));
-        diamonds += Math.round(Math.pow(i*10, 0.7));
-        uranium += Math.round(Math.pow(i*30, 1.5));
+        money += Math.round(Math.pow(i * 6000, 1.5));
+        gold += Math.round(Math.pow(i * 180, 1.5));
+        oil += Math.round(Math.pow(i * 30, 1.5));
+        ore += Math.round(Math.pow(i * 25, 1.5));
+        diamonds += Math.round(Math.pow(i * 10, 0.7));
+        uranium += Math.round(Math.pow(i * 30, 1.5));
     }
 
     for (let i = 1; i <= aero; i++) {
-        money += Math.round(Math.pow(i*1000, 1.5));
-        gold += Math.round(Math.pow(i*180, 1.5));
-        oil += Math.round(Math.pow(i*10, 1.5));
-        ore += Math.round(Math.pow(i*10, 1.5));
-        diamonds += Math.round(Math.pow(i*10, 0.7));
+        money += Math.round(Math.pow(i * 1000, 1.5));
+        gold += Math.round(Math.pow(i * 180, 1.5));
+        oil += Math.round(Math.pow(i * 10, 1.5));
+        ore += Math.round(Math.pow(i * 10, 1.5));
+        diamonds += Math.round(Math.pow(i * 10, 0.7));
     }
 
     for (let i = 1; i <= hfound; i++) {
-        money += Math.round(Math.pow(i*30, 1.5));
-        gold += Math.round(Math.pow(i*216, 1.5));
-        oil += Math.round(Math.pow(i*16, 1.5));
-        ore += Math.round(Math.pow(i*9, 1.5));
+        money += Math.round(Math.pow(i * 30, 1.5));
+        gold += Math.round(Math.pow(i * 216, 1.5));
+        oil += Math.round(Math.pow(i * 16, 1.5));
+        ore += Math.round(Math.pow(i * 9, 1.5));
     }
 
     console.log(`Всего ресурсов затрачено:\nРубли ${(money / 1e+12).toFixed(3)}T\nЗолото ${(gold / 1e+12).toFixed(3)}T\nНефть ${(oil / 1e+9).toFixed(3)}ККК\nРуда ${(ore / 1e+9).toFixed(3)}ККК\nАлмазы ${(diamonds / 1e+3).toFixed(3)}К\nУран ${(uranium / 1e+6).toFixed(3)}КК`);
 
-    return {money: money, gold: gold, oil: oil, ore: ore, uranium: uranium, diamonds: diamonds};
+    return { money: money, gold: gold, oil: oil, ore: ore, uranium: uranium, diamonds: diamonds };
 }
